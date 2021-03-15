60f87c4bee0c6a27fff057c737e13a6226e9bd7c
patriciagpereira89@gmail.com <patriciagpereira89@gmail.com>
2021-03-14 23:54:39 -0300

    Estruturado estilo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="Quadrado">
+        <div className="Qdentro">
+          <div id="0.0" className="qq">
+            0.0
+          </div>
+          <div id="1.0" className="qq">
+            1.0
+          </div>
+          <div id="2.0" className="qq">
+            2.0
+          </div>
+          <div id="3.0" className="qq">
+            3.0
+          </div>
+        </div>
+        <div className="Qdentro">
+          <div id="0.1" className="qq">
+            0.1
+          </div>
+          <div id="1.1" className="qq">
+            1.1
+          </div>
+          <div id="2.1" className="qq">
+            2.1
+          </div>
+          <div id="3.1" className="qq">
+            3.1
+          </div>
+        </div>
+        <div className="Qdentro">
+          < div id="0.2" className="qq">
+            0.2
+          </ div>
+          <div id="1.2" className="qq">
+            1.2
+          </div>
+          <div id="2.2" className="qq">
+            2.2
+          </div>
+          <div id="3.2" className="qq">
+            3.2
+          </div>
+        </div>
+        <div className="Qdentro">
+          <div id="0.3" className="qq">
+            0.3
+          </div>
+          <div id="1.3" className="qq">
+            1.3
+          </div>
+          <div id="2.3" className="qq">
+            2.3
+          </div>
+          <div id="3.3" className="qq">
+            3.3
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
