41f50f7c0a5a1226756561d60817e5e016c1c264
Patricia Pereira <patriciagpereira89@gmail.com>
2021-05-01 11:21:03 -0300

    atualizado tabuleiro

--- src/App.js
+++ src/App.js
@@ -9,41 +9,44 @@ function soma_Cores(cor1, cor2) {
     } else {
       return 0
     }
-  } else if(cor1 === 7 && cor2 ===7){
+  } else if (cor1 === 7 && cor2 === 7) {
     return 8
-  } else if (cor1 === 8 && cor2 === 8){
+  } else if (cor1 === 8 && cor2 === 8) {
     return 9
-  }else if (cor1 === 9 && cor2 === 9){
+  } else if (cor1 === 9 && cor2 === 9) {
     return 9
-  } 
+  }
   else {
     return 0
   }
-}
-
+}//fecha funcao soma_cores
+const dirs = { CIMA: 0, BAIXO: 1, DIREITA: 2, ESQUERDA: 3 }
 function tabuleiro(i, j, direcao) {
   var linha
   var coluna
-  if (direcao === "ArrowLeft") {
-    linha = i
+  switch (direcao) {
+    case dirs.ESQUERDA :
+      linha = i
     coluna = j
-
+      break;
+  case dirs.DIREITA:
+  linha = i
+  coluna = 3 - j
+    break;
+case dirs.CIMA:
+  linha = j
+  coluna = i
+  break;
+  case dirs.BAIXO:
+  linha = 3 - j
+  coluna = i
+  break;
+    default:
+      break;
   }
-  if (direcao === "ArrowRight") {
-    linha = i
-    coluna = 3 - j
-
-  }
-  if (direcao === "ArrowUp") {
-    linha = j
-    coluna = i
-  }
-  if (direcao === "ArrowDown") {
-    linha = 3 - j
-    coluna = i
-  }
+ 
   return [linha, coluna]
-}
+}//fim funcao tabuleiro
 
 class App extends React.Component {
   constructor(props) {
@@ -52,36 +55,41 @@ class App extends React.Component {
     //estado inicial
     this.state = {
       quadrados: [[3, 1, 2, 3], [4, 5, 6, 7], [8, 2, 4, 1], [2, 3, 4, 5]]
-     // quadrados: [[0, 0, 0, 0], [7, 7, 0, 0], [0, 0, 0, 8], [0, 0, 0, 0]]
+      // quadrados: [[0, 0, 0, 0], [7, 7, 0, 0], [0, 0, 0, 8], [0, 0, 0, 0]]
     }
   }
 
-handleBnt = (event) => {
-this.setState((state) => {
-  var quadrados = [[3, 1, 2, 3], [4, 5, 6, 7], [8, 2, 4, 1], [2, 3, 4, 5]]
-  return {quadrados}
-})
+  handleBnt = (event) => {
+    this.setState((state) => {
+      var quadrados = [[3, 1, 2, 3], [4, 5, 6, 7], [8, 2, 4, 1], [2, 3, 4, 5]]
+      return { quadrados }
+    })
 
-}
+  }
 
-
-handleTeclado = (event) => {
-  const dirs = {CIMA:0, BAIXO:1, DIREITA:2, ESQUERDA:3}
-  switch (event.code) {
-    case "ArrowDown":
-        atualizaTabuleiro(dirs.BAIXO)
-    case "ArrowUp":
-      atualizaTabuleiro(dirs.CIMA)
-     case "ArrowLeft":
-       atualizaTabuleiro(dirs.ESQUERDA)
+  handleTeclado = (event) => {
+    
+    switch (event.code) {
+      case "ArrowDown":
+        this.atualizaTabuleiro(dirs.BAIXO)
+        break
+      case "ArrowUp":
+        this.atualizaTabuleiro(dirs.CIMA)
+        break
+      case "ArrowLeft":
+        this.atualizaTabuleiro(dirs.ESQUERDA)
+        break
       case "ArrowRight":
-        atualizaTabuleiro(dirs.DIREITA)  
-  }    
-  return
-}
+        this.atualizaTabuleiro(dirs.DIREITA)
+        break
+        default:
+          break 
+    }
+    return
+  }//fim handleTeclado
 
-atualizaTabuleiro = (direcao) => {
-        
+  atualizaTabuleiro = (direcao) => {
+
     this.setState((state) => {
       var quadrados_novos = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
@@ -89,8 +97,8 @@ atualizaTabuleiro = (direcao) => {
         var k = 0
         for (var j = 0; j < 4; j++) {
           var linha_atual, coluna_atual, coluna_nova, linha_nova
-          [linha_atual, coluna_atual] = tabuleiro(i, j, event.code);
-          [linha_nova, coluna_nova] = tabuleiro(i, k, event.code)
+          [linha_atual, coluna_atual] = tabuleiro(i, j, direcao);
+          [linha_nova, coluna_nova] = tabuleiro(i, k, direcao)
 
           var cor_atual = state.quadrados[linha_atual][coluna_atual]
           var cor_nova = quadrados_novos[linha_nova][coluna_nova]
@@ -103,7 +111,7 @@ atualizaTabuleiro = (direcao) => {
               cor_soma = soma_Cores(cor_atual, cor_nova);
               if (cor_soma === 0) {
                 cor_soma = cor_atual;
-                [linha_nova, coluna_nova] = tabuleiro(i, k, event.code)
+                [linha_nova, coluna_nova] = tabuleiro(i, k, direcao)
 
               }
 
@@ -117,17 +125,19 @@ atualizaTabuleiro = (direcao) => {
       return { quadrados: quadrados_novos }
     })
 
+
+  }//fim atualiza 
+
   
-  }
 
   componentDidMount() {
     document.querySelector(".App").focus()
   }
 
   render() {
-    
+
     return (
-     
+
       <div className="App" onKeyDown={this.handleTeclado} tabIndex="0" >
 
         <div id="Quadrado">
@@ -188,11 +198,10 @@ atualizaTabuleiro = (direcao) => {
               {this.state.quadrados[3][3]}
             </div>
           </div>
-        </div> 
+        </div>
         <button onClick={this.handleBnt} id='btn'>Reiniciar </button>
       </div>//fim div app 
-     );
-    }
+    );
   }
 }
 
